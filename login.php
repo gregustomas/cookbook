ef0cc2857e91ebe6dc4e3cf1220c4f43a1f04a23
Tomáš Greguš <gregustomas@seznam.cz>
2025-02-06 16:15:19 +0100

    Upravil jsem vzhled login a register formulářů

--- login.php
+++ login.php
@@ -2,29 +2,9 @@
     session_start();
     include('db.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-<form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
-        <h2>Přihlášení</h2>
-        username:
-        <input type="text" name="jmeno">
-        <br>
-        heslo:
-        <input type="password" name="heslo">
-        <br>
-        <button type="submit" name="submit" value="login">Přihlásit se</button>
-    </form>
-</body>
-</html>
-
 <?php
+    $error_message = "";
+
     //security filtr
     $username = filter_input(INPUT_POST, "jmeno", FILTER_SANITIZE_SPECIAL_CHARS);
     $password = filter_input(INPUT_POST, "heslo", FILTER_SANITIZE_SPECIAL_CHARS);
@@ -40,30 +20,72 @@
         $result = mysqli_stmt_get_result($stmt); //vrací objekt s informacemi z databáze
 
         if(empty($username)){
-            echo "Zadejte jméno";
+            $error_message = "Zadej jméno.";
         }
         else if(empty($password)){
-            echo "Zadejte heslo";
+            $error_message = "Zadej heslo.";
         }
         else{
-        }
-        if($user = mysqli_fetch_assoc($result)){ //pokud se našli jakékoliv data v $result tak se vykoná podmínka, data = nazvy sloupce a hodnoty k nim v sql
-            if(password_verify($password, $user['heslo'])){ //ověření hesla
-                //nastavení session - úspěšné přihlášení
-                $_SESSION['username'] = $username;
-                $_SESSION['password'] = $password;
-                echo "Přihlášení proběhlo úspěšně";
-                mysqli_close($conn);
-                header('Location: dashboard.php');
-                exit;
+            if($user = mysqli_fetch_assoc($result)){ //pokud se našli jakékoliv data v $result tak se vykoná podmínka, data = nazvy sloupce a hodnoty k nim v sql
+                if(password_verify($password, $user['heslo'])){ //ověření hesla
+                    //nastavení session - úspěšné přihlášení
+                    $_SESSION['username'] = $username;
+                    $_SESSION['password'] = $password;
+                    echo "Přihlášení proběhlo úspěšně";
+                    mysqli_close($conn);
+                    header('Location: dashboard.php');
+                    exit;
+                } 
+                else {
+                    $error_message = "Zadali jste nesprávné heslo.";
+                }
             } 
             else {
-                echo "Zadali jste nesprávné heslo.";
-            }
-        } 
-        else {
-            echo "Uživatel neexistuje.";
-        }   
+                $error_message = "Uživatel neexistuje.";
+            } 
+        }
+          
     }
     mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+</head>
+<body>
+    
+    <div class="container-sm d-flex justify-content-center mt-4">
+        <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST"
+        class="bg-dark text-white py-4 px-5 rounded w-100" style="max-width: 500px;">
+        <h2>Log in</h2>  
+
+        <div class="mb-3">
+                <label for="userInput" class="form-label">Jméno</label>
+                <input type="text" class="form-control" id="userInput" name="jmeno">
+            </div>
+            <div class="mb-3">
+                <label for="passwordInput" class="form-label">Password</label>
+                <input type="password" class="form-control" id="passwordInput" name="heslo">
+            </div>
+            <button type="submit" class="btn btn-primary">Log in</button>
+
+            <?php if (!empty($error_message)): ?>
+                <div class="alert alert-danger mt-3 text-center"><?php echo $error_message; ?></div>
+            <?php endif; ?>
+        </form>
+    </div>
+
+</body>
+</html>
+
--- register.php
+++ register.php
@@ -1,31 +1,8 @@
 <?php
     include('db.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrace</title>
-</head>
-<body>
-
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
-        <h2>Registrace</h2>
-        username:
-        <input type="text" name="jmeno">
-        <br>
-        heslo:
-        <input type="password" name="heslo">
-        <br>
-        <button type="submit" name="submit" value="register">Zaregistrovat se</button>
-    </form>
-
-</body>
-</html>
-
 <?php
+    $error_message = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         //security filtr
@@ -34,10 +11,10 @@
     
         //opatření prázdných hodnot
         if(empty($username)){
-            echo "Zadejte jméno";
+            $error_message = "Zadejte jméno";
         }
         else if(empty($password)){
-            echo "Zadejte heslo";
+            $error_message = "Zadejte heslo";
         }
         else{
             //hash hesla, pokud jsou údaje OK a insert do db
@@ -53,7 +30,7 @@
                 exit;
             }
             catch(mysqli_sql_exception){
-                echo "Toto jméno již někdo používá :(";
+                $error_message = "Toto jméno již někdo používá :(";
             }
             
         }
@@ -61,3 +38,44 @@
 
     mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrace</title>
+
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+
+</head>
+<body>
+
+    <div class="container-sm d-flex justify-content-center mt-4">
+        <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST"
+        class="bg-dark text-white py-4 px-5 rounded w-100" style="max-width: 500px;">
+        <h2>Log in</h2>  
+
+        <div class="mb-3">
+                <label for="userInput" class="form-label">Jméno</label>
+                <input type="text" class="form-control" id="userInput" name="jmeno">
+            </div>
+            <div class="mb-3">
+                <label for="passwordInput" class="form-label">Password</label>
+                <input type="password" class="form-control" id="passwordInput" name="heslo">
+            </div>
+            <button type="submit" class="btn btn-primary" value="register">Register</button>
+        
+            <?php if (!empty($error_message)): ?>
+                <div class="alert alert-danger mt-3 text-center"><?php echo $error_message; ?></div>
+            <?php endif; ?>
+        </form>
+    </div>
+
+</body>
+</html>
